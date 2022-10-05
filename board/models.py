401ff957935ac1e01390810175d657c79d4e1d08
kidsage <ruhyun92@gmail.com>
2022-10-05 19:23:16 +0900

    10/5 pass

--- board/models.py
+++ board/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserInformation(models.Model):
+    nickname = models.CharField(max_length=10)
+
+
+
+class Match(models.Model):
+    pass
