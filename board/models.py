632af5df523bd59c4c59ced5a1a616062d8cbb47
kidsage <ruhyun92@gmail.com>
2022-10-06 22:14:58 +0900

    10/6 piongg 제대로 시작

--- board/models.py
+++ board/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class UserInformation(models.Model):
     nickname = models.CharField(max_length=10)
-
+    access_ai = models.CharField(max_length=100)
+    level = models.PositiveIntegerField(null=True)
 
 
 class Match(models.Model):
