2d02e4feed71b52ac1215e129e0f7e2894bf5f62
kidsage <ruhyun92@gmail.com>
2022-10-10 23:04:31 +0900

    10/10 fifaonline api get 테스트 중.

--- datahub/fionApi.py
+++ datahub/fionApi.py
@@ -24,6 +24,7 @@ class FionApi:
         self._matchtype = matchtype
         self._offset = 0
         self._limit = 100
+        self._orderby = 'desc'
 
     ## Get metadata
     def MetaData(self, type: str):
@@ -32,6 +33,7 @@ class FionApi:
         '''
         req = requests.get(f'https://static.api.nexon.co.kr/fifaonline4/latest/{type}.json', headers=self._headers)
         data = req.json()
+
         return data
 
     def GetActionShot(self, id: int):
@@ -45,31 +47,60 @@ class FionApi:
         except Exception as e: # error 발생 시 normal shot
             req = requests.get(f'https://fo4.dn.nexoncdn.co.kr/live/externalAssets/common/players/p{id}.png', headers=self._headers)
             data = req.json()
+
         return data
 
     ## Get user data
     def UserInfomation(self):
         req = requests.get(f'https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname={self._nickname}', headers=self._headers)
         data = req.json()
+
         return data
 
     def UserMatchInformation(self):
         api = FionApi(self._nickname)
         api_data = api.UserInfomation()
 
-        req = requests.get(f'https://api.nexon.co.kr/fifaonline4/v1.0/users/{api_data["accessid"]}\
+        req = requests.get(f'https://api.nexon.co.kr/fifaonline4/v1.0/users/{api_data["accessid"]} \
                             /matches?matchtype={self._matchtype}&offset={self._offset}&limit={self._limit}',
                             headers=self._headers)
         data = req.json()
+
         return data
 
+    def UserMaxDivision(self):
+        api = FionApi(self._nickname)
+        api_data = api.UserInfomation()
+        
+        req = requests.get(f'https://api.nexon.co.kr/fifaonline4/v1.0/users/{api_data["accessid"]}/maxdivision', headers=self._headers)
+        data = req.json()
+
+        return data
+
+    def UserTradeData(self, tradetype: str):
+        req = requests.get(f'https://api.nexon.co.kr/fifaonline4/v1.0/users/{api_data["accessid"]}/ \
+                            markets?tradetype={tradetype}&offset={self._offset}&limit={self._limit}', 
+                            headers=self._headers)
+        data = req.json()
+
+        return data
+
+    ## User Match Data
+    def UserMatchData(self, type: str):
+        '''
+        type = 'matchtype', 'spid', 'seasonid', 'spposition', 'division', 'division_volta' 중 한개 선택
+        '''
+        req = requests.get(f'https://api.nexon.co.kr/fifaonline4/v1.0/matches?matchtype={type} \
+                            &offset={self._offset}&limit={self._limit}&orderby={self._orderby}',
+                            headers=self._headers)
 
 
 
-    
 # if __name__ == '__main__':
 #     api = FionApi('매기구이')
 #     print(api.UserInfomation())
 
-
-# decorator를 쓰기에는 좀 복잡한데 차라리 상속을 받아 쓰는게 나은가?
+"""
+10/10 일단 api에서 기본으로 제공하는 데이터는 가져오는 것 확인
+10/11 modeling 이후 view까지 테스트 하는 것으로 생각하고 진행할 예정.
+"""
